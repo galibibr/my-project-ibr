fbd2a517663f698411eba971e0eae0f9fd7af497
GalibIbr <galibiavi4@gmail.com>
2024-04-20 17:59:43 +0300

    Solved the first task

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,36 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from "./data.js";
 
-console.log("Let's rock")
-console.log(encoded, translations)
+// Создаём функцию для расшифровки
+const decoding = (encoded, translations) => {
+   // Пройдём по всем элементам шифрованного массива
+   return encoded.map((obj) => {
+      // Пройдём по всем свойствам (ключам) элемента (объекта) массива
+      for (const key in obj) {
+         // Исключаем некоторые поля и оставим в изначальном виде
+         if (
+            key === "groupId" ||
+            key === "service" ||
+            key === "formatSize" ||
+            key === "ca"
+         ) {
+            continue;
+         }
+         /* Если находим значение обходяемого объекта из объекта для 
+         перевода, то меняем значение обходяемого объекта на значение
+         найденного из объекта для перевода */
+         else if (obj[key] in translations) {
+            obj[key] = translations[obj[key]];
+         }
+      }
+      /* После того как расшифровали все нужные значения пройденного 
+      объекта, возвращаем расшифрованный обеъкт в массив */
+      return obj;
+   });
+};
+// Присваеваем расшифрованный массив к новой перемнной decoded
+const decoded = decoding(encoded, translations);
 
+console.log("Let's rock");
+console.log(encoded, translations);
 
-
-// console.log(decoded)
+console.log(decoded);
